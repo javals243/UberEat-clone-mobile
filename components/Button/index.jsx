e69814e74166cd45453e66e506305ef5cb794a81
javals243 <vchihisa@gmail.com>
2022-10-09 16:23:11 +0200

    initial commit

---
+++ components/Button/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Pressable, StyleSheet, Text } from "react-native";
+
+const Button = ({ text, onPress, containerStyles }) => {
+  return (
+    <Pressable onPress={onPress} style={[styles.root, containerStyles]}>
+      <Text style={styles.text}>{text}</Text>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    backgroundColor: "#e47911",
+    marginVertical: 10,
+    height: 35,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: "#a15e1b",
+  },
+  text: {
+    fontSize: 16,
+  },
+});
+
+export default Button;
